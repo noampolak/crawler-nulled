3a884434fb73544ca32a3d8f1be6ae455fa8c985
Noam <noampolak@gmail.com>
2022-10-13 07:21:18 +0300

    improve spider

--- crawler/crawler/spiders/nulled_vip_leaks.py
+++ crawler/crawler/spiders/nulled_vip_leaks.py
@@ -9,10 +9,10 @@ from .. import settings
 import random
 
 user_agent_list = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
+    # 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+    # 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
+    # 'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    # 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363',
 ]
 
@@ -34,8 +34,10 @@ class NulledVipLeaksSpider(CrawlSpider):
                 cookies={
                     "nulledmember_id": 5417320,
                     "nulledpass_hash": "6f3ded8072218af80cf3ec77e4f05cb4",
-                    "nulledsession_id": "ae977c65994a677fa6551c39bf9dc891",
-                    "User-Agent": user_agent_list[random.randint(0, len(user_agent_list)-1)]
+                    "nulledsession_id": "36b94141227aeed9bc5d6fbeb07fc064",
+                    # "User-Agent": user_agent_list[random.randint(0, len(user_agent_list)-1)]
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363"
+
                 }
             )
 
